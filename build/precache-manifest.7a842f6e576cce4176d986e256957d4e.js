3ea03b2ecb735f8fa14c6f2bb079067ee5a28cab
Anushka-K24 <anushka.kalamkar24@gmail.com>
2021-04-24 20:14:58 +0530

    footer added

---
+++ build/precache-manifest.7a842f6e576cce4176d986e256957d4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1937e60acbd6e8ef18fd6c7fb0e60221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d94aef646680e15e2cc2",
+    "url": "/static/css/main.9e41198b.chunk.css"
+  },
+  {
+    "revision": "7c695c6fef4daf746c30",
+    "url": "/static/js/2.2450c8c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2450c8c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94aef646680e15e2cc2",
+    "url": "/static/js/main.bc5282a1.chunk.js"
+  },
+  {
+    "revision": "20f8afee3c99b2b29ebd",
+    "url": "/static/js/runtime-main.90925036.js"
+  }
+]);
